c8d8c57971dc0fa795013d16804addea64221629
laudeon <louis.audeon@mail.be>
2019-06-19 14:30:24 +0200

    finished last step

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 const App = (function (my) {
 
-  my.run = function () {
-    console.log('App loaded...')
+  my.hello = function () {
+    console.log('Hello !')
+  }
+
+  my.lanceMoi = function () {
+    console.log("nolpmis/yl.dnarber//:sptth".split("").reverse().join(""))
+    console.log("Ceci est la présentation de la toute nouvelle version du site web de Simplon en développement à la prod en ce moment ! Enjoy !")
+    console.log("Tadaaaa ! T'es arrivé⋅e jusqu'au bout ! Toutes mes féliciations ! Tu peux te faire appeler Hackeur⋅se dorénavent :p")
   }
 
   return my
